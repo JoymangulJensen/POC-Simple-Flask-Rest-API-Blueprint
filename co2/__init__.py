f89c8352db6f994992912428063a4d7afd50cb36
Younes OUFRID <oufridyounes@gmail.com>
2021-03-29 14:58:40 +0100

    💡 add comments to co2 module (feature)

--- co2/__init__.py
+++ co2/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+# creation of new co2 blueprint with /co2 prefix
 co2_blueprint = Blueprint('co2', __name__, url_prefix='/co2')
 
 # import routes
--- co2/routes.py
+++ co2/routes.py
@@ -1,17 +1,18 @@
-# Pressure
+# CO2
 from flask import jsonify
 from flask_api import status
 
+# import co2 blueprint and Co2 model
 from co2 import co2_blueprint
 from co2.models import Co2
 
-
+# retreive all co2's data 
 @co2_blueprint.route('')
 def get_co2s():
     co2s = Co2.query.all()
     return jsonify(co2s), status.HTTP_200_OK
 
-
+# get co2 data by co2 id
 @co2_blueprint.route("<int:co2_id>", methods=['GET'])
 def get_co2_by_id(co2_id):
     co2 = Co2.query.filter_by(co2_id=co2_id).first()
