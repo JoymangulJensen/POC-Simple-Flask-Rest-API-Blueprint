b0cbc2adda5eb5e243a6a0a8f70b4cac95fbd89d
Jensen Joymangul <jensen.joymangul@univ-lyon2.fr>
2021-03-29 13:54:59 +0200

    ✨ Add noise URI

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from app.utils import CustomJSONEncoder
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,7 +9,13 @@ def create_app(config):
     # create application instance
     app = Flask(__name__)
     app.config.from_object(config)
+    app.json_encoder = CustomJSONEncoder
 
+    
     db.init_app(app)
+
+    from noise import noise_blueprint
+
+    app.register_blueprint(noise_blueprint)
 
     return app
---
+++ noise/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+noise_blueprint = Blueprint('noise', __name__, url_prefix='/noise')
+
+# import routes
+from . import routes
