0c0156c45610777d28720fdd9a14a22130b27197
Younes OUFRID <oufridyounes@gmail.com>
2021-03-29 14:58:40 +0100

    💡 add comments to co2 module (feature)

--- co2/models.py
+++ co2/models.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 import datetime
 from app import db
 
-
+# our Co2 model 
 @dataclass
 class Co2(db.Model):
     __tablename__ = 'co2'
-
+# attributes of Co2 model
     co2_id: int
     co2_lib: str
     value: float
@@ -17,7 +17,7 @@ class Co2(db.Model):
     creation_time:  datetime.time
     experience_id: int
     materiel_id: int
-
+# mapping Class Co2 and table co2
     co2_id = db.Column(db.Integer(), primary_key=True)
     co2_lib = db.Column(db.String())
     value = db.Column(db.Float())
