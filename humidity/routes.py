9a8c7975c47ef2ef3a8dfff9604b97992806d55e
Shaima <shaima.shalbi@esprit.tn>
2021-03-30 11:14:28 +0200

    :construction: update problems

--- humidity/routes.py
+++ humidity/routes.py
@@ -7,6 +7,6 @@ from humidity.models import Humidity
 
 
 @humidity_blueprint.route('')
-def get_humiditys():
+def get_humidities():
     pressures = Humidity.query.all()
     return jsonify(pressures), status.HTTP_200_OK
