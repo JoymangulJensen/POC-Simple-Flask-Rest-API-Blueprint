83d883a05cd16a72982aa55da7af4a622eb02842
Jensen Joymangul <jensen.joymangul@univ-lyon2.fr>
2021-04-04 20:01:56 +0200

    ⚡️Create route to get only one noise tuple

--- noise/routes.py
+++ noise/routes.py
@@ -7,3 +7,10 @@ from noise.models import Noise
 def get_noises():
     pressures = Noise.query.all()
     return jsonify(pressures), status.HTTP_200_OK
+
+@noise_blueprint.route("<int:noise_id>", methods=['GET'])
+def get_co2_by_id(noise_id):
+    noise = Noise.query.filter_by(noise_id=noise_id).first()
+    if noise is not None:
+        return jsonify(noise), status.HTTP_200_OK
+    return jsonify(noise), status.HTTP_409_CONFLICT
