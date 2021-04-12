bb372b41a6ec85a65faeb5414644bcd6badd5f44
Jensen Joymangul <jensen.joymangul@univ-lyon2.fr>
2021-04-13 01:26:28 +0200

    🚑️ Add critical routes for temp and humidity

--- humidity/routes.py
+++ humidity/routes.py
@@ -1,7 +1,7 @@
 # Pressure
 from flask import jsonify
 from flask_api import status
-
+from flask_api import status
 from humidity import humidity_blueprint
 from humidity.models import Humidity
 
@@ -10,3 +10,10 @@ from humidity.models import Humidity
 def get_humidities():
     pressures = Humidity.query.all()
     return jsonify(pressures), status.HTTP_200_OK
+
+@humidity_blueprint.route("<int:humidity_id>", methods=['GET'])
+def get_co2_by_id(humidity_id):
+    humidity = Humidity.query.filter_by(humidity_id=humidity_id).first()
+    if humidity is not None:
+        return jsonify(humidity), status.HTTP_200_OK
+    return jsonify(humidity), status.HTTP_409_CONFLICT
--- temperature/routes.py
+++ temperature/routes.py
@@ -1,6 +1,6 @@
 # Temperature
 from flask import jsonify
-
+from flask_api import status
 from temperature import temperature_blueprint
 from temperature.models import Temperature
 
@@ -9,3 +9,10 @@ from temperature.models import Temperature
 def get_temperature():
     temperatures = Temperature.query.all()
     return jsonify(temperatures)
+
+@temperature_blueprint.route("<int:temperature_id>", methods=['GET'])
+def get_co2_by_id(temperature_id):
+    temperatures = Temperature.query.filter_by(temperature_id=temperature_id).first()
+    if temperatures is not None:
+        return jsonify(temperatures), status.HTTP_200_OK
+    return jsonify(temperatures), status.HTTP_409_CONFLICT
